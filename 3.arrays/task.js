7e16639c36a8d860360adc3e470131891b788470
WarManDev <warmandevelopment@gmail.com>
2022-09-18 21:58:37 +0400

    home work v1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,7 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result = arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 
   return result; // boolean
 }
@@ -9,7 +9,7 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
