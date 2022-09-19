4917a2007f5bd0107bcc93bf1785227a23408ad8
WarManDev <warmandevelopment@gmail.com>
2022-09-19 19:37:14 +0400

    home work v1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+new Student('Вася', 'мужской', 20);
+new Student('Ваня', 'мужской', 22);
+new Student('Маша', 'женский', 18);
+
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  this.marks = [];
+  for(let mark of marks) {
+    this.marks.push(mark);
+  }
+  // for(let i = 0; i < marks.length; i++) {
+  //   this.marks.push(marks[i]);
+  // }
+}
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  let avgMark;
+  for(let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  return avgMark = sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
